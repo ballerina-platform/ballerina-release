b537345a0dca422c7bf604e3b98040b060478055
niveathika <niveathika@gmail.com>
2021-04-29 22:10:15 +0530

    Remove unnecessary version initalisation

--- dependabot/update_version_data.py
+++ dependabot/update_version_data.py
@@ -1,6 +1,5 @@
 import json
 import os
-import re
 import sys
 import time
 
@@ -23,7 +22,7 @@ def main():
     module_name_list = sort_module_name_list()
     print('Fetched module name list')
     module_details_json = initialize_module_details(module_name_list)
-    print('Initialized module details and fetched latest module versions')
+    print('Initialized module details')
     module_details_json = get_immediate_dependents(module_name_list, module_details_json)
     print('Fetched immediate dependents of each module')
     module_details_json = calculate_levels(module_name_list, module_details_json)
@@ -84,24 +83,6 @@ def get_dependencies(module_name):
             dependencies.append(module)
 
     return dependencies
-
-
-# Gets the version of the ballerina extension module from gradle.properties file in module repository
-# returns: current version of the module
-def get_version(module_name):
-    repo = github.get_repo(constants.BALLERINA_ORG_NAME + '/' + module_name)
-    properties_file = repo.get_contents(constants.GRADLE_PROPERTIES_FILE)
-    data = properties_file.decoded_content.decode(constants.ENCODING)
-
-    version = ''
-    for line in data.splitlines():
-        if re.match('version=', line):
-            version = line.split('=')[-1]
-
-    if version == '':
-        print('Version not defined for ' + module_name)
-
-    return version
 
 
 # Gets the default branch of the extension repository
@@ -202,7 +183,6 @@ def initialize_module_details(modules_list):
     }
 
     for module in modules_list:
-        version = get_version(module['name'])
         default_branch = get_default_branch(module['name'])
 
         artifact_name = module['name'].split('-')[-1]
@@ -212,7 +192,6 @@ def initialize_module_details(modules_list):
 
         module_details_json['modules'].append({
             'name': module['name'],
-            'version': version,
             'level': 0,
             'group_id': module.get('group_id', 'org.ballerinalang'),
             'artifact_id': module.get('artifact_id', default_artifact_id),
