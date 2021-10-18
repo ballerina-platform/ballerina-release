bb35cc37961b0e3085d82676f1a5a19582302a49
Charuka Tharindu <tharinduu@wso2.com>
2021-10-18 13:00:03 +0530

    Add EM tagging

--- dependabot/notify_connector_failure.py
+++ dependabot/notify_connector_failure.py
@@ -6,7 +6,7 @@ import sys
 import csv
 
 code_owners = open("CODEOWNERS", "r")
-owner = code_owners.read().split("*")[1].split("@")[-1].strip()
+owners = code_owners.read().split("*")[1].split("@")
 
 encryption_key = os.environ['ENV_USER_ENCRYPTION_KEY']
 
@@ -18,11 +18,16 @@ decrypted = fernet.decrypt(encrypted_csv)
 with open('dependabot/resources/github_users_decrypted.csv', 'wb') as dec_file:
     dec_file.write(decrypted)
 
-with open('dependabot/resources/github_users_decrypted.csv', 'r') as read_obj:
-    user_file = csv.DictReader(read_obj)
-    for row in user_file:
-        if row['gh-username'] == owner:
-            code_owner_id = row['user-id']
+message = "*" + str(sys.argv[1]) + "* daily build failure" + "\n" +\
+          "Please visit <https://github.com/ballerina-platform/" + str(sys.argv[1]) + "/actions?query=workflow%3A%22Daily+build%22|the daily build page> for more information" +"\n"
+
+for owner in owners :
+    with open('dependabot/resources/github_users_decrypted.csv', 'r') as read_obj:
+        user_file = csv.DictReader(read_obj)
+        owner = owner.strip()
+        for row in user_file:
+            if row['gh-username'] == owner:
+                message += "<users/" + row['user-id'] + ">" + "\n"
 
 build_chat_id = os.environ['ENV_NOTIFICATIONS_CHAT_ID']
 build_chat_key = os.environ['ENV_NOTIFICATIONS_CHAT_KEY']
@@ -31,9 +36,6 @@ build_chat_token = os.environ['ENV_NOTIFICATIONS_CHAT_TOKEN']
 url = 'https://chat.googleapis.com/v1/spaces/' + build_chat_id + \
           '/messages?key=' + build_chat_key + '&token=' + build_chat_token
 
-message = "*"+str(sys.argv[1])+"* daily build failure" + "\n" +\
-          "Please visit <https://github.com/ballerina-platform/"+str(sys.argv[1])+"/actions?query=workflow%3A%22Daily+build%22|the daily build page> for more information" +"\n"+\
-          "<users/" + code_owner_id + ">"
 chat_message = {"text": message}
 message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
