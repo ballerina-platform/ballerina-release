f92d4fd6cb7a5b17462e2da69d5873cded6e8f5d
niveathika <niveathika@gmail.com>
2022-01-11 23:48:46 +0530

    Skip wait for ballerina observe codecov

--- dependabot/update_dependencies_in_pipeline.py
+++ dependabot/update_dependencies_in_pipeline.py
@@ -309,7 +309,7 @@ def check_pending_pr_checks(index: int):
             if module['auto_merge'] & ('AUTO MERGE' in pull_request.title):
                 # Distribution does not have codecov checks, nether does ballerinai/observe
                 if not (module['name'] == 'ballerina-distribution' or module['name'] == 'module-ballerinai-observe'
-                    or codecov_complete):
+                    or module['name'] == 'module-ballerina-observe' or codecov_complete):
                     # Wait till the codecov checks pass before merge
                     return
                 try:
