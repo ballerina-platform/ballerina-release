61180204caffc2046c200092de27c9630e558461
NipunaMadhushan <nipuna.madhushan96@gmail.com>
2022-05-04 13:36:29 +0530

    Enable c2c tests

--- dependabot/build_stdlibs_for_lang_updates.py
+++ dependabot/build_stdlibs_for_lang_updates.py
@@ -139,7 +139,7 @@ def build_stdlib_repositories(enable_tests):
                 os.system(f"cd {module['name']}/ballerina-tests;" +
                           "find . -name \"Dependencies.toml\" -delete;")
 
-            if module['name'] == "module-ballerina-graphql" or module['name'] == "module-ballerina-c2c":
+            if module['name'] == "module-ballerina-graphql":
                 exit_code = os.system(f"cd {module['name']};" +
                                       f"export packageUser={ballerina_bot_username};" +
                                       f"export packagePAT={ballerina_bot_token};" +
