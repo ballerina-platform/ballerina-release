510c2016f9c641667f9dc8a02e47e8810447c68b
NipunaMadhushan <nipuna.madhushan96@gmail.com>
2022-05-27 09:05:51 +0530

    Change branch for websubhub module

--- dependabot/full_build_pipeline_for_updated_stages.py
+++ dependabot/full_build_pipeline_for_updated_stages.py
@@ -243,6 +243,15 @@ def switch_to_branches_from_updated_stages():
                           f"{module['name']}")
                     sys.exit(1)
                 continue
+            elif module['name'] == "module-ballerina-websubhub":
+                os.system(f"echo {module['name']}")
+                exit_code = os.system(f"cd {module['name']};git checkout 2201.0.x")
+
+                if exit_code != 0:
+                    print(f"Failed to switch to branch 'v1.0.x' from last updated commit id for " +
+                          f"{module['name']}")
+                    sys.exit(1)
+                continue
             try:
                 version = properties[module['version_key']]
                 if len(version.split("-")) > 1:
